ea0b3cc282f211b583e1d2434ef636e7efe1dded
Nataly <97055353+Nataliya1508@users.noreply.github.com>
2023-01-28 23:48:44 +0200

    add news endpoint

---
+++ routes/newsRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const { getAllNews } = require("../controllers/getAllNews");
+
+const { asyncWrapper } = require('../helpers/apiHelpers');
+
+
+
+const router = express.Router();
+
+router.get('/news', asyncWrapper(getAllNews));
+
+module.exports = router;
