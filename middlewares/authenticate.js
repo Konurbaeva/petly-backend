e14ba07a43b2e91dbda717c1de8a5a3ff8790eb6
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2023-01-28 15:28:17 +0100

    added middleware that will check the user is authorized or not

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -6,3 +6,28 @@ const { User } = require("../models/userModel")
 require("dotenv").config()
 
 const { SECRET_KEY } = process.env
+
+const authenticate = async(req, res, next) => {
+    const { authorization } = req.headers
+    const [bearer, token] = authorization.split(" ")
+
+    if(bearer!== "Bearer"){
+        next(RequestError(401))
+    }
+    try{
+       const { id } = jwt.verify(token, SECRET_KEY)
+       const user = await User.findById(id)
+
+        if(!user || !user.token){
+            next(RequestError(401))
+        }
+
+        req.user = user
+        next()
+
+    } catch(error) {
+      next(RequestError(401))
+    }
+}
+
+module.exports = authenticate;
