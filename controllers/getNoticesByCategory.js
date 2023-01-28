81a9088146164eb71c8b6e56b80d78543224e2bb
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2023-01-28 12:16:57 +0100

    added RequestError to handle errors

--- controllers/getNoticesByCategory.js
+++ controllers/getNoticesByCategory.js
@@ -1,13 +1,11 @@
-// const getNoticesByCategory = categoryName => {
-//     return Notices.find({ categoryName });
-//   }
-  
+const { RequestError } = require("../helpers")
+
 const getNoticesByCategory = async(categoryName) => {
     const {categoryName} = req.params
     const result = await Notices.find({ categoryName })
 
     if(!result) {
-        throw Error(404, "Not found")
+        throw RequestError(404, "Not found")
     }
     res.json(result)
   }
