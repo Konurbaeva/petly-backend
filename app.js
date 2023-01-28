c00e60502ecfa5356f0a62a6bed20289139610f4
Alexey <alexei20293@yahoo.com>
2023-01-28 12:28:55 +0200

    connected DB

--- app.js
+++ app.js
@@ -6,9 +6,8 @@ const contactsRouter = require('./routes/api/contacts')
 
 const app = express()
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
-app.use(logger(formatsLogger))
+app.use(logger('short'))
 app.use(cors())
 app.use(express.json())
 
