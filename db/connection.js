e099fef4671df1b79dd03f75bd8a7ee09b15d212
Alexey <alexei20293@yahoo.com>
2023-01-28 13:05:26 +0200

    create userModel, create user base route, create register endpoint

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 mongoose.set("strictQuery", false);
 
 const connectMongo = async () => {
-    console.log(process.env.DB_HOST);
+    // console.log(process.env.DB_HOST);
     return mongoose.connect(process.env.DB_HOST);
 }
 
