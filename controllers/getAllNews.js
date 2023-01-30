d3d6f6c4ecfbcc32cf461c6d4a3952164bc16337
Nataly <97055353+Nataliya1508@users.noreply.github.com>
2023-01-30 15:34:07 +0200

    add error

--- controllers/getAllNews.js
+++ controllers/getAllNews.js
@@ -3,7 +3,10 @@ const News = require("../models/newsModel");
 
 const getAllNews = async (req, res, next) => {
     const result = await News.find()
-  
+
+  if (!result) {
+    throw RequestError(404, "Not found");
+  }
 
     res.json(result)
 }
