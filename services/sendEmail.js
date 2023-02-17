3fc0a373dc2ebf950e5d6a7de1e98102ba401061
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2023-02-17 09:59:21 +0100

    init commit

--- services/sendEmail.js
+++ services/sendEmail.js
@@ -5,7 +5,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendEmail = async (data) => {
     try {
-        const email = {...data, from: "olexiy.solotvinskiy@gmail.com"}
+        const email = {...data, from: "s0539451@htw-berlin.de"}
         await sgMail.send(email);
         console.log('Email send successfull');
     } catch (error) {
